fc769500ab2a38e22c544008fb3b4bdf1a61e5b4
jmigual <j.marce.igual@gmail.com>
2017-03-14 19:03:14 +0100

    Added configure script

---
+++ configure.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+usage() {
+	echo "Usage: $0 [-a|-p]"
+	echo "-a  Configure all (virtualenv and python packages)"
+}
+
+all=false
+while getopts ":a" o; do
+    case "${0}" in 
+        a)
+            all=true
+            ;;
+        *)
+            usage
+            exit
+            ;;
+    esac
+done
+
+if [ ${all} = true ]; then
+    echo Installing fluidsynth
+    sudo apt install fluidsynth -y
+
+    echo Installing virtual environment
+    pip3 install virtualenv
+    virtualenv .
+fi
+
+source bin/activate
+
+echo ========================================================
+echo To finish the virtualenv installation you need to execute
+echo source bin/activate
+echo ========================================================
