53e1033a89b5c4c506bb81339c8572ae482cd26c
jmigual <j.marce.igual@gmail.com>
2017-04-28 12:16:23 +0200

    Fix installation

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 # Custom script to install all the dependencies to a raspberry
 sudo apt install git -y
 git clone https://github.com/jmigual/projecte2
-./projecte2/Code/configure.sh -a
+./projecte2/Code/configure.sh
